47d3f8ff070004b61187aa6362a42529a6e4a76d
Laurence Tratt <laurie@tratt.net>
2023-05-16 10:32:22 +0100

    We no longer need to build ykllvm separately.

--- .buildbot.sh
+++ .buildbot.sh
@@ -10,29 +10,6 @@ cd ..
 make clean
 
 # Now test again with the JIT enabled.
-#
-# First we build ykllvm.
-git clone --depth 1 https://github.com/ykjit/ykllvm
-cd ykllvm
-mkdir build
-cd build
-# For why we turn off PIE:
-# https://github.com/llvm/llvm-project/issues/57085
-cmake -DCMAKE_INSTALL_PREFIX=`pwd`/../inst \
-    -DLLVM_INSTALL_UTILS=On \
-    -DCMAKE_BUILD_TYPE=release \
-    -DLLVM_ENABLE_ASSERTIONS=On \
-    -DLLVM_ENABLE_PROJECTS="lld;clang" \
-    -DCLANG_DEFAULT_PIE_ON_LINUX=OFF \
-    -DBUILD_SHARED_LIBS=ON \
-    -GNinja \
-    ../llvm
-cmake --build .
-cmake --install .
-export PATH=`pwd`/../inst/bin:${PATH}
-cd ../..
-
-# Then we build Yk.
 export CARGO_HOME="`pwd`/.cargo"
 export RUSTUP_HOME="`pwd`/.rustup"
 export RUSTUP_INIT_SKIP_PATH_CHECK="yes"
@@ -44,7 +21,7 @@ sh rustup.sh --default-host x86_64-unknown-linux-gnu \
     -y
 export PATH=`pwd`/.cargo/bin/:$PATH
 
-git clone --depth 1 https://github.com/softdevteam/yk
+git clone --recurse-submodules --depth 1 https://github.com/softdevteam/yk
 cd yk && cargo build
 export PATH=`pwd`/ykcapi/scripts:${PATH}
 cd ..
