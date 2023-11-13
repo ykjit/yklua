982420b3ea918bc1e7f5fd361953bf2683e020ce
Pavel Durov <pavel@p3ld3v.dev>
2023-11-13 16:34:23 +0000

    Enable serialised only tests.

--- test.sh
+++ test.sh
@@ -4,25 +4,8 @@ set -e
 
 cd tests
 
-# YKFIXME: The JIT can't yet run the test suite, but the following commented
-# commands are what we are aiming at having work.
-#
-# ../src/lua -e"_U=true" all.lua
+# YKFIXME: The YK JIT can't yet run the test suite with non-serialised compilation (YKD_SERIALISE_COMPILATION=0).
 # YKD_SERIALISE_COMPILATION=1 ../src/lua -e"_U=true" all.lua
-#
-# (Adding `YKD_SERIALISE_COMPILATION` exercises different threading behaviour
-# that could help to shake out more bugs)
-#
-# Until we can run `all.lua` reliably, we just run the tests that we know to
-# run within reasonable time).
 
-LUA=../src/lua
-
-for serialise in 0 1; do
-    for test in api bwcoercion closure code events \
-                gengc pm tpack tracegc vararg goto \
-                cstack locals literals; do
-        echo "### YKD_SERIALISE_COMPILATION=$serialise $test.lua ###"
-        YKD_SERIALISE_COMPILATION=$serialise ${LUA} ${test}.lua
-    done
-done
+# Running tests with a serialised compilation
+YKD_SERIALISE_COMPILATION=1 ../src/lua -e"_U=true" all.lua
