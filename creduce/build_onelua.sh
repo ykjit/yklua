92f464557e7905d82d3a4ad1d33b613a06ee5943
Pavel Durov <pavel@p3ld3v.dev>
2023-11-14 11:07:36 +0000

    Add c-vice scripts and single lua source file compilation. These scripts will help us to find bugs in large code bases such as lua. It will iteratively reduce the size of the codebase using c-vice and script oracles.

---
+++ creduce/build_onelua.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -eu
+
+if ! command -v cvise 2>&1 ; then
+    echo "Binary 'cvise' is not accessible in the system or not in the PATH."
+    echo "See installation instructions: https://github.com/marxin/cvise."
+    exit 1
+fi
+
+if [ -d "./src" ]; then
+    rm -fr "./src"
+fi
+
+cp -r "../src" "./src"
+cp "Makefile" "./src/Makefile"
+cp ./cvise.example.sh "./src/cvise.example.sh"
+cp "./one.c" "./src/one.c"
+
+cd "./src"
+
+make onelua
---
+++ creduce/cvise.example.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "${YKLUA_HOME}" ]; then
+    echo "YKLUA_HOME environment variable is not defined."; 
+    exit 1
+fi
+
+if [ -z "${YK_BUILD_TYPE}" ]; then
+    echo "YK_BUILD_TYPE environment variable is not defined."; 
+    exit 1
+fi
+
+if ! command -v yk-config 2>&1 ; then
+    echo "yk-config is not found."
+    echo "See installation instructions: https://github.com/marxin/cvise."
+    exit 1
+fi
+
+cp -r "${YKLUA_HOME}/creduce/src" ./
+cd ./src
+
+make onelua
+
+# oracle
+./onelua -e "print('hello world')" | grep "hello world"
