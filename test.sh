9b1f36d718560eb6b184047f8a2f107328bfab79
Pavel Durov <pavel@p3ld3v.dev>
2023-11-24 09:25:25 +0000

    Set serialisation off for non-serialised compilation tests.

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -e
 cd tests
 
 # YKFIXME: The YK JIT can't yet run the test suite with non-serialised compilation (YKD_SERIALISE_COMPILATION=0).
-# YKD_SERIALISE_COMPILATION=1 ../src/lua -e"_U=true" all.lua
+# YKD_SERIALISE_COMPILATION=0 ../src/lua -e"_U=true" all.lua
 
 # Running tests with a serialised compilation
 YKD_SERIALISE_COMPILATION=1 ../src/lua -e"_U=true" all.lua
