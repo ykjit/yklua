c3a0bd4b234c2b5c238ce7f6e6b7ffa32550d0d9
Edd Barrett <vext01@gmail.com>
2024-06-07 13:35:36 +0100

    Ensure we use the correct reduced onelua.c file.
    cvise puts the candidate reduced file in a temp dir and then it's up to
    you to test it. We were copying in the source tree, but without
    installing the candidate onelua.c file, so this wasn't doing what we
    thought it was.

--- creduce/cvise.example.sh
+++ creduce/cvise.example.sh
@@ -18,7 +18,12 @@ if ! command -v yk-config 2>&1 ; then
     exit 1
 fi
 
+# Rebuild with the reduced onelua.c in-place.
+#
+# Note that it's not enough to just copy onelua.c and `Makefile` in. The whole
+# `src` dir is needed.
 cp -r "${YKLUA_HOME}/creduce/src" ./
+cp onelua.c src/
 cd ./src
 
 make onelua
